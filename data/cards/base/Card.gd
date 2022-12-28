6533772a3cd51b11a29ff4fb37282d8676452a41
Andropas <andrii.boikiv.ik.2021@lpnu.ua>
2022-12-28 12:49:49 +0200

    Initial commit

---
+++ data/cards/base/Card.gd
@@ -0,0 +1,17 @@
+extends Button
+
+signal buy_card
+signal use_card
+signal card_selected
+signal card_unselected
+
+export var price = 0
+
+onready var gold_label = $Stats/Header/Gold
+onready var game = get_tree().root.get_child(0)
+
+func _ready():
+	gold_label.text = str(price) + "g"
+
+func _on_Card_button_down():
+	pass # Replace with function body.
---
+++ data/game/Game.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+var selected_card
+
+func _on_card_selected(card):
+	selected_card = card
